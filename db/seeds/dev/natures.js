7f9911730041d0992379d68323ead4bfbcb79f49
Allie McCarthy <mccarthy.allie@gmail.com>
2020-01-30 14:29:15 -0700

    Seed tables with real data

--- db/seeds/dev/natures.js
+++ db/seeds/dev/natures.js
@@ -1,23 +1,61 @@
-const naturesData = require('../../../papersData');
+const naturesData = require('../../../naturesData');
+const pokemonsData = require('../../../pokemonsData');
 
-exports.seed = async function (knex) {
+
+const createNatures = async (knex, nature) => {
+  const natureId = await knex('natures').insert({
+    name: nature.name,
+    good: nature.good,
+    bad: nature.bad
+  }, 'id');
+
+  const pokemonList = pokemonsData.results.filter(poke => {
+    return poke.nature === nature.name
+  })
+
+  let pokePromises = pokemonList.map(poke => {
+    return knex('pokemon').insert({
+      name: poke.name,
+      nature_id: natureId[0]
+    })
+  })
+
+  return Promise.all(pokePromises);
+};
+
+exports.seed = async (knex) => {
   try {
-    await knex('natures').del() // delete all footnotes first
-    await knex('pokemon').del() // delete all papers
+    await knex('pokemon').del()
+    await knex('natures').del()
 
-    // Now that we have a clean slate, we can re-insert our paper data
-    // Insert a single paper, return the paper ID, insert 2 footnotes
-    const naturesId = await knex('natures').insert({
-      name: 'Fooo', good: 'Yay', bad: 'Argh'
-    }, 'id')
-    return knex('pokemon').insert([
-      { name: 'Lorem', nature_id: naturesId[0] },
-      { name: 'Dolor', nature_id: naturesId[0] }
-    ])
+    let naturePromises = naturesData.results.map(nature => {
+      return createNatures(knex, nature);
+    });
+
+    return Promise.all(naturePromises);
   } catch (error) {
-    console.log(`Error seeding data: ${error}`);
+    console.log(`Error seeding data: ${error}`)
   }
-}
+};
+
+// exports.seed = async function (knex) {
+//   try {
+//     await knex('natures').del() // delete all footnotes first
+//     await knex('pokemon').del() // delete all papers
+//
+//     // Now that we have a clean slate, we can re-insert our paper data
+//     // Insert a single paper, return the paper ID, insert 2 footnotes
+//     const naturesId = await knex('natures').insert({
+//       name: 'Fooo', good: 'Yay', bad: 'Argh'
+//     }, 'id')
+//     return knex('pokemon').insert([
+//       { name: 'Lorem', nature_id: naturesId[0] },
+//       { name: 'Dolor', nature_id: naturesId[0] }
+//     ])
+//   } catch (error) {
+//     console.log(`Error seeding data: ${error}`);
+//   }
+// }
 
 
 
