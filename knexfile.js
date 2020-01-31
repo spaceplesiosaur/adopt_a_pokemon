4f140ed884e7c4b2936e517a36c632f22df883e5
Allie McCarthy <mccarthy.allie@gmail.com>
2020-01-31 09:37:19 -0700

    Update knexfile for heroku

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/pokepairs',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
       directory: './db/migrations'
     },
